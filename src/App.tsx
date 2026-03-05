9bb33b2db540b4cef7fdeaebf250965386cd0c30
Fur1ouS <Fur1ouS@ya.ru>
2026-03-05 19:04:08 +0300

    add useState add array music add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 
-export function App() {
-  const [count, setCount] = useState(0)
+type Tracks = {
+  id: number,
+  title: string,
+  url: string,
+}
 
-  const hendlClick = () =>{
-    setCount(count + 1)
+const tracksMusic = [
+  {
+    id: 1,
+    title: 'One track',
+    url: 'https://www.google.com/',
   }
+]
+
+export function App() {
+  const [tracks, setTracks] = useState<Tracks[] | null>(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTracks(tracksMusic)
+    }, 1000)
+  },[])
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={hendlClick}>
-          count is {count}
-        </button>
-      </div>
+      <h1>Music Tracks</h1>
+      {tracks === null && <span>loading...</span>}
+      {tracks?.length === 0 && <span>Треков Нет</span>}
+      <ul>
+        {tracks?.map(track => {
+          return <li key={track.id}>
+            <h5>{track.title}</h5>
+            <audio src={track.url} controls={true}></audio>
+          </li>
+        })}
+      </ul>
     </>
   )
 }
