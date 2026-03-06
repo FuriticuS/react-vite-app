442e8c3434519c2f8455c74e8b5c74c1864d5417
Fur1ouS <Fur1ouS@ya.ru>
2026-03-06 21:43:40 +0300

    add click for track and add image and text song

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,34 @@ type Tracks = {
   attachments: TrackAttachments
 }
 
+type TrackImagesMain = {
+  url: string,
+}
+
+type TrackImages = {
+  main: TrackImagesMain[]
+}
+
+type TrackDetailsAttributes = {
+  title: string,
+  attributes: string,
+  lyrics: string,
+  attachments: TrackAttachments,
+  images: TrackImages
+}
+
+type TrackDetailsResource = {
+  id: string,
+  attributes: TrackDetailsAttributes
+}
+
 export function App() {
   const [tracks, setTracks] = useState<Tracks[] | null>(null)
-  const [selectedTrackId, setselectedTrackId] = useState<string | null>(null)
+  const [selectedTrackId, setSelectedTrackId] = useState<string | null>(null)
+  const [selectedTrack, setSelectedTrack] = useState<TrackDetailsResource | null>(null)
 
   useEffect(() => {
-    fetch(`https://musicfun.it-incubator.app/api/1.0/playlists/tracks`, {
+    fetch(`https://musicfun.it-incubator.app/api/1.0/playlists/tracks?pageSize=5`, {
       headers: {
         'api-key': 'be114072-4e9d-4dfa-9bbb-23bfd7a4e3b1'
       }
@@ -40,7 +62,21 @@ export function App() {
       .then(json => {
         setTracks(json.data)
       })
-  },[])
+  }, [])
+
+  function handleSelectTrack(trackId: string) {
+    setSelectedTrackId(trackId)
+
+    fetch(`https://musicfun.it-incubator.app/api/1.0/playlists/tracks/` + trackId, {
+      headers: {
+        'api-key': 'be114072-4e9d-4dfa-9bbb-23bfd7a4e3b1'
+      }
+    })
+      .then(result => result.json())
+      .then(json => {
+        setSelectedTrack(json.data)
+      })
+  }
 
   return (
     <>
@@ -50,16 +86,35 @@ export function App() {
       <ul>
         {tracks?.map(track => {
           const style: CSSProperties = {}
-          if(track.id === selectedTrackId) {
+          if (track.id === selectedTrackId) {
             style.border = '1px solid orange'
           }
 
+          const handleSelect = () => {
+            handleSelectTrack(track.id)
+          }
+
           return <li key={track.id} style={style}>
-            <h5 onClick={() => setselectedTrackId(track.id)}>{track.attributes.title}</h5>
+            <h5 onClick={handleSelect}>{track.attributes.title}</h5>
             <audio src={track.attributes.attachments[0].url} controls={true}></audio>
           </li>
         })}
       </ul>
+      <hr/>
+      {selectedTrackId && !selectedTrack && <span>Loading...</span>}
+      {selectedTrack && <div>
+        <h3>Track Details for {selectedTrack.attributes.title}</h3>
+        {selectedTrack?.attributes?.images?.main?.[0]?.url?.trim()
+          ? (
+            <img
+              src={selectedTrack.attributes.images.main[0].url}
+              alt={selectedTrack?.attributes?.title ?? "Обложка"}
+            />
+          )
+          : "Нет картинки"
+        }
+        <p>{selectedTrack.attributes.lyrics}</p>
+      </div>}
     </>
   )
 }
